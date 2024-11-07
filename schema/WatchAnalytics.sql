82e5022fe71c523941f72a8acd1b9cb71dd6954e
wisemoon <willyedoo@gmail.com>
2024-11-07 09:44:24 +0100

    Update WatchAnalytics.sql
    pstgresql test

--- schema/WatchAnalytics.sql
+++ schema/WatchAnalytics.sql
@@ -1,57 +1,48 @@
+CREATE TABLE watch_tracking_user (
+    tracking_timestamp BYTEA NOT NULL,
+    user_id            INTEGER NOT NULL,
+    num_watches        INTEGER NOT NULL,
+    num_pending        INTEGER NOT NULL,
+    event_notes        VARCHAR(63)
+);
 
-CREATE TABLE /*_*/watch_tracking_user (
-	tracking_timestamp  VARBINARY(14) NOT NULL,
-	user_id             INT(10) NOT NULL,
-	num_watches         INT(10) NOT NULL,
-	num_pending         INT(10) NOT NULL,
-	event_notes			VARCHAR(63)
-) /*$wgDBTableOptions*/;
+CREATE INDEX watch_tracking_user_datapoint ON watch_tracking_user (tracking_timestamp, user_id);
+CREATE INDEX watch_tracking_user_user ON watch_tracking_user (user_id);
+CREATE INDEX watch_tracking_user_timestamp ON watch_tracking_user (tracking_timestamp);
 
-CREATE INDEX /*i*/watch_tracking_user_datapoint ON /*_*/watch_tracking_user (tracking_timestamp,user_id);
-CREATE INDEX /*i*/watch_tracking_user_user      ON /*_*/watch_tracking_user (user_id);
-CREATE INDEX /*i*/watch_tracking_user_timestamp ON /*_*/watch_tracking_user (tracking_timestamp);
+CREATE TABLE watch_tracking_page (
+    tracking_timestamp BYTEA NOT NULL,
+    page_id            INTEGER NOT NULL,
+    num_watches        INTEGER NOT NULL,
+    num_reviewed       INTEGER NOT NULL,
+    event_notes        VARCHAR(63)
+);
 
+CREATE UNIQUE INDEX watch_tracking_page_datapoint ON watch_tracking_page (tracking_timestamp, page_id);
+CREATE INDEX watch_tracking_page_page ON watch_tracking_page (page_id);
+CREATE INDEX watch_tracking_page_timestamp ON watch_tracking_page (tracking_timestamp);
 
-CREATE TABLE /*_*/watch_tracking_page (
-	tracking_timestamp  VARBINARY(14) NOT NULL,
-	page_id             INT(10) NOT NULL,
-	num_watches         INT(10) NOT NULL,
-	num_reviewed        INT(10) NOT NULL,
-	event_notes			VARCHAR(63)
-) /*$wgDBTableOptions*/;
+CREATE TABLE watch_tracking_wiki (
+    tracking_timestamp          BYTEA NOT NULL,
+    num_pages                   INTEGER NOT NULL,
+    num_watches                 INTEGER NOT NULL,
+    num_pending                 INTEGER NOT NULL,
+    max_pending_minutes         INTEGER NOT NULL,
+    avg_pending_minutes         INTEGER NOT NULL,
+    num_unwatched               INTEGER NOT NULL,
+    num_one_watched             INTEGER NOT NULL,
+    num_unreviewed              INTEGER NOT NULL,
+    num_one_reviewed            INTEGER NOT NULL,
+    content_num_pages           INTEGER NOT NULL,
+    content_num_watches         INTEGER NOT NULL,
+    content_num_pending         INTEGER NOT NULL,
+    content_max_pending_minutes INTEGER NOT NULL,
+    content_avg_pending_minutes INTEGER NOT NULL,
+    content_num_unwatched       INTEGER NOT NULL,
+    content_num_one_watched     INTEGER NOT NULL,
+    content_num_unreviewed      INTEGER NOT NULL,
+    content_num_one_reviewed    INTEGER NOT NULL,
+    event_notes                 VARCHAR(63)
+);
 
-CREATE UNIQUE INDEX /*i*/watch_tracking_page_datapoint ON /*_*/watch_tracking_page (tracking_timestamp,page_id);
-CREATE INDEX        /*i*/watch_tracking_page_page      ON /*_*/watch_tracking_page (page_id);
-CREATE INDEX        /*i*/watch_tracking_page_timestamp ON /*_*/watch_tracking_page (tracking_timestamp);
-
-
-CREATE TABLE /*_*/watch_tracking_wiki (
-	tracking_timestamp          VARBINARY(14) NOT NULL,
-	
-	num_pages                   INT(10) NOT NULL,
-	num_watches                 INT(10) NOT NULL,
-	num_pending                 INT(10) NOT NULL,
-	max_pending_minutes         INT(10) NOT NULL,
-	avg_pending_minutes         INT(10) NOT NULL,
-
-	num_unwatched               INT(10) NOT NULL,
-	num_one_watched             INT(10) NOT NULL,
-	num_unreviewed              INT(10) NOT NULL,
-	num_one_reviewed            INT(10) NOT NULL,
-
-	content_num_pages           INT(10) NOT NULL,
-	content_num_watches         INT(10) NOT NULL,
-	content_num_pending	        INT(10) NOT NULL,
-	content_max_pending_minutes INT(10) NOT NULL,
-	content_avg_pending_minutes INT(10) NOT NULL,
-
-	content_num_unwatched       INT(10) NOT NULL,
-	content_num_one_watched     INT(10) NOT NULL,
-	content_num_unreviewed      INT(10) NOT NULL,
-	content_num_one_reviewed    INT(10) NOT NULL,
-
-	event_notes			        VARCHAR(63)
-) /*$wgDBTableOptions*/;
-
-
-CREATE UNIQUE INDEX /*i*/watch_tracking_wiki_datapoint ON /*_*/watch_tracking_wiki (tracking_timestamp);
+CREATE UNIQUE INDEX watch_tracking_wiki_datapoint ON watch_tracking_wiki (tracking_timestamp);
